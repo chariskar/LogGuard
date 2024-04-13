f1b594e84b875c8ae0643b7743a833fed89892b0
Harriskar <charkarametos@gmail.com>
2024-04-13 22:53:21 +0300

    added redability

--- logger.py
+++ logger.py
@@ -59,14 +59,16 @@ class Logger:
         # get thread lock
         self.lock = None
         try:
+            
             self.lock = threading.Lock()
+            
         except Exception as e:
+            
             raise Errors.UnableToLock(f"Unable to get process lock with error {e}")
         
         self.loglevel = LogLevel.upper()
         
         #initialise variables
-        self.settings = None
         self.file_path = None
         self.log_file = None
         self.log_file_name = None
@@ -75,18 +77,23 @@ class Logger:
         # assign values
         self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         self.supported_formats = ["log", "txt"]
-        
+        self.configured_level_value = self.settings['LogLevels'].get(self.loglevel)
         
         #check format type
         if log_file_type in self.supported_formats:
+            
             self.log_file_type = log_file_type
         else:
+            
             raise ValueError("Log file type isn't supported")
         
         # check for the output files
         if output_dir == ".":
+            
             self.file_path = Path.cwd().resolve()
+            
         else:
+            
             self.file_path = Path(output_dir).resolve()
 
         # create the log file
@@ -94,14 +101,20 @@ class Logger:
         
         #check the settings path
         self.settings_path = settings_path
+        
         if self.settings_path == './log_settings.json':
+            
             with open(self.settings_path, 'r') as f:
+                
                 self.settings = json.load(f)
         else:
             if Path(self.settings_path).resolve():
+                
                 with open(self.settings_path, 'r') as f:
+                    
                     self.settings = json.load(f)
             else:
+                
                 raise Errors.PathNonExistant('File path does not exist')
             
     @staticmethod
@@ -135,17 +148,26 @@ class Logger:
             UnableToLock: If the script cant acquire a thread lock
         """
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")  # get the timestamp
+        
         message = self.format(message)  # format the args
+        
         level = level.upper()  # make the level upper case if it isn't
 
         if self.lock:
+            
             with self.lock:
+                
                 if self.log_file:  # if the log file exists
+                    
                     if self.settings:  # if the self.settings variable is not None
+                        
                         log_level_value = self.settings['LogLevels'].get(level)
-                        configured_level_value = self.settings['LogLevels'].get(self.loglevel)
-                        if log_level_value is not None and configured_level_value is not None:
-                            if log_level_value >= configured_level_value:
+                        
+                        if log_level_value is not None and self.configured_level_value is not None: 
+                            # check that the configed log level is lower than the give
+        
+                            if log_level_value >= self.configured_level_value:
+                                
                                 if not context:
                                     self.log_file.write(
                                         f"[{timestamp}] [{level}] {message}\n"
@@ -173,18 +195,24 @@ class Logger:
             PathNonExistant: If the file path is not found
         """
         self.log_file_name = self.get_log_name()  # get the log file name
+        
         if self.file_path:
+            
             self.file_path.mkdir(parents=True, exist_ok=True)  # make the log dir if it doesn't exist
         else:
             raise Errors.PathNonExistant("File path not found")  # raise an error if file path is  None
         try:
             if self.log_file_name in Logger.open_loggers:  # check if there is an already open logger file
+                
                 self.log_file = Logger.open_loggers[self.log_file_name]  # open that logger file
             else:
                 self.log_file = open(str(self.log_file_name), "a")  # if there isn't an open logger file make one
+                
                 Logger.open_loggers[self.log_file_name] = self.log_file  # open that file
+                
                 self.log('info', "Starting")  # set a starting message
         except IOError:
+            
             sys.stderr.write(f"Error: Unable to open log file {self.log_file_name}\n")
 
     def get_log_name(self):
@@ -210,13 +238,22 @@ class Logger:
             FileNotOpen: if file is not open.
         """
         if self.log_file:
+            
             try:
+                
                 self.log('info', "Closing file")
+                
             finally:
+                
                 self.log_file.close()
+                
                 with self.open_loggers_lock:
+                    
                     if self.log_file_name in self.open_loggers:
+                        
                         del self.open_loggers[self.log_file_name]
+                        
                 self.log_file = None
         else:
+            
             raise Errors.FileNotOpen("Log file is not open")
