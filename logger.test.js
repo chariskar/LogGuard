f61e08934c5121a1a4ad64d10a07afa9f9ca0c17
Harriskar <charkarametos@gmail.com>
2024-04-21 16:42:23 +0300

    small changes

--- logger.test.js
+++
@@ -1,5 +0,0 @@
-import * as Logger from './logger.js'
-
-const logger = new Logger.Logger()
-
-logger.log('info','hi')
--- logger.test.ts
+++
@@ -1,4 +0,0 @@
-import * as Logger from './logger'
-const logger = new Logger.Logger()
-
-logger.log('info','hi')
